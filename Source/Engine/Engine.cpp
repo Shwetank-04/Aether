fadbe01746064b6b9f50e83e1d5ab8774d151df0
Shwetank-04 <shwetankvats10@gmail.com>
2026-07-25 18:00:26 +0530

    Update Engine.cpp

--- Source/Engine/Engine.cpp
+++ Source/Engine/Engine.cpp
@@ -1,1 +1,29 @@
+#include "Engine.h"
 
+void Engine::prepare(double sampleRate,
+                     int samplesPerBlock)
+{
+    voiceManager.prepare(sampleRate,
+                         samplesPerBlock);
+}
+
+void Engine::noteOn(int note,
+                    float velocity)
+{
+    voiceManager.noteOn(note,
+                        velocity);
+}
+
+void Engine::noteOff(int note)
+{
+    voiceManager.noteOff(note);
+}
+
+void Engine::process(float* left,
+                     float* right,
+                     int numSamples)
+{
+    voiceManager.render(left,
+                        right,
+                        numSamples);
+}
