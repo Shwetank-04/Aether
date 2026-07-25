1a83d3b32362134c628e2b8674c41f1bee0a1de5
Shwetank-04 <shwetankvats10@gmail.com>
2026-07-25 18:01:48 +0530

    Update MainView.h

--- GUI/MainView.h
+++ GUI/MainView.h
@@ -1,1 +1,17 @@
+#pragma once
 
+class MainView
+{
+public:
+    MainView();
+
+    void initialize();
+
+    void resize(int width,
+                int height);
+
+private:
+
+    int windowWidth = 1280;
+    int windowHeight = 720;
+};
