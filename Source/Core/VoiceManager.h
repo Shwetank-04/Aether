ae38af393c1364a0f0ed49e02f5b58e93fc2263a
Shwetank-04 <shwetankvats10@gmail.com>
2026-07-25 17:45:29 +0530

    Update VoiceManager.h

--- Source/Core/VoiceManager.h
+++ Source/Core/VoiceManager.h
@@ -1,1 +1,19 @@
+#pragma once
 
+#include <vector>
+#include "Voice.h"
+
+class VoiceManager
+{
+public:
+    void prepare(double sampleRate, int samplesPerBlock);
+
+    void noteOn(int midiNote, float velocity);
+
+    void noteOff(int midiNote);
+
+    void render(float* left, float* right, int numSamples);
+
+private:
+    std::vector<Voice> voices;
+};
