139fba73d8bbf9b1908b842a2bca5729875b00e5
Shwetank-04 <shwetankvats10@gmail.com>
2026-07-25 18:02:47 +0530

    Create Layout.h

---
+++ GUI/Layout.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct Layout
+{
+    static constexpr int HeaderHeight = 70;
+
+    static constexpr int FooterHeight = 35;
+
+    static constexpr int BrowserWidth = 280;
+
+    static constexpr int MinWidth = 960;
+
+    static constexpr int MinHeight = 540;
+
+    static constexpr int DefaultWidth = 1280;
+
+    static constexpr int DefaultHeight = 720;
+};
