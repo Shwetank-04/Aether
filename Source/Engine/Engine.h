06cc4af8775e464199ddf6d0682d17a16428beb0
Shwetank-04 <shwetankvats10@gmail.com>
2026-07-25 18:00:05 +0530

    Update Engine.h

--- Source/Engine/Engine.h
+++ Source/Engine/Engine.h
@@ -1,1 +1,22 @@
+#pragma once
 
+#include "VoiceManager.h"
+
+class Engine
+{
+public:
+    void prepare(double sampleRate,
+                 int samplesPerBlock);
+
+    void noteOn(int note,
+                float velocity);
+
+    void noteOff(int note);
+
+    void process(float* left,
+                 float* right,
+                 int numSamples);
+
+private:
+    VoiceManager voiceManager;
+};
