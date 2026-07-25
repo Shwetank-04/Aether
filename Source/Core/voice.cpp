f7d121d14798b123991595f20ee888aec2404929
Shwetank-04 <shwetankvats10@gmail.com>
2026-07-25 17:44:48 +0530

    Update voice.cpp

--- Source/Core/voice.cpp
+++ Source/Core/voice.cpp
@@ -1,1 +1,32 @@
+#include "Voice.h"
 
+Voice::Voice()
+{
+}
+
+void Voice::prepare(double sr, int)
+{
+    sampleRate = sr;
+}
+
+void Voice::noteOn(int midiNote, float velocity)
+{
+    currentNote = midiNote;
+    currentVelocity = velocity;
+    active = true;
+}
+
+void Voice::noteOff()
+{
+    active = false;
+}
+
+void Voice::render(float*, float*, int)
+{
+    // Audio generation will come later.
+}
+
+bool Voice::isActive() const
+{
+    return active;
+}
